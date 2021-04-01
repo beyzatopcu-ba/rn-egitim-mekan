174fd12d1767f8cb7235981d2f48d58f76033869
beyzatopcu-ba <beyza.topcu@bilgeadam.com>
2021-04-01 15:18:42 +0300

    button on homescreen to go to restaurants

--- src/Screens/Home/HomeScreen.js
+++ src/Screens/Home/HomeScreen.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 const HomeScreen = props => {
+
+    const _onPress_GoToRestaurants = () => {
+        props.navigation.navigate("restaurants-screen");
+    }
+
+
     return (
         <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>HomeScreen'im ben</Text>
+            <TouchableOpacity onPress={_onPress_GoToRestaurants}>
+                <Text>Restoran ekranına git</Text>
+            </TouchableOpacity>
         </View>
     )
 }
