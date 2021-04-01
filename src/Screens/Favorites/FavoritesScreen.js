4526d1a0f978e58cf7e80cea6e3ec94b366aabf7
beyzatopcu-ba <beyza.topcu@bilgeadam.com>
2021-04-01 14:05:23 +0300

    added screens

---
+++ src/Screens/Favorites/FavoritesScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const FavoritesScreen = props => {
+    return (
+        <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>FavoritesScreen</Text>
+        </View>
+    )
+}
+
+export default FavoritesScreen;
