84f74a5f4e37840d26bafb968e6a1d99614340b4
beyzatopcu-ba <beyza.topcu@bilgeadam.com>
2021-04-02 11:51:56 +0300

    added and integrated fonts

---
+++ react-native.config.js
@@ -0,0 +1,7 @@
+// Font eklemek için, fontları ekleyip bu dosyayı oluşturduktan sonra
+// Terminalde proje yolundayken, 'react-native link' komutunu çalıştırıyoruz.
+module.exports = {
+    // Dosya yolunun fontları içeren klasörü vermesi gerek
+    // En sonra / koymayı unutmayın
+    assets: ['./src/Assets/Fonts/'],
+};
