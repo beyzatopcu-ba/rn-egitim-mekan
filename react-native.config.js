3c7a6aa875747a821da7f1bf52420c8b23461ff7
beyzatopcu-ba <beyza.topcu@bilgeadam.com>
2021-04-02 11:53:52 +0300

    added comment

--- react-native.config.js
+++ react-native.config.js
@@ -1,5 +1,7 @@
 // Font eklemek için, fontları ekleyip bu dosyayı oluşturduktan sonra
 // Terminalde proje yolundayken, 'react-native link' komutunu çalıştırıyoruz.
+// Font eklendikten sonra uygulama ilk defa git'ten çekildiğinde
+// bu komutu tekrar çalıştırmak gerekebilir.
 module.exports = {
     // Dosya yolunun fontları içeren klasörü vermesi gerek
     // En sonra / koymayı unutmayın
